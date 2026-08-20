5ad5a17f3a3dfff697c1e84425e91541403b4219
denarelys-lgtm <denarelys@gmail.com>
2026-08-20 16:16:20 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/detectcamera/MainActivity.java
+++ app/src/main/java/com/example/detectcamera/MainActivity.java
@@ -42,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtIpStatus = findViewById(R.id.txtIpStatus);
+        int resId = getResources().getIdentifier("txtIpStatus", "id", getPackageName());
+        if (resId != 0) {
+            txtIpStatus = findViewById(resId);
+        }
 
         Shizuku.addRequestPermissionResultListener(shizukuListener);
 
